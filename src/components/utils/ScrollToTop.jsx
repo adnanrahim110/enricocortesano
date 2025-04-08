3c2afb41efc008e99060cdec555b81c53cfeb3a3
adnan <adnankaka.0092@gmail.com>
2025-04-09 00:08:09 +0500

    commit

--- src/components/utils/ScrollToTop.jsx
+++ src/components/utils/ScrollToTop.jsx
@@ -28,16 +28,16 @@ const ScrollToTop = () => {
   return (
     <div
       onClick={scrollToTop}
-      className={`fixed z-[100] bottom-1 right-0 bg-gray-800 border-2 border-primary-100 hover:bg-black text-primary transition-all duration-200 ease-linear cursor-pointer group ${
+      className={`fixed z-[100] bottom-1 right-1 bg-gray-800 border-2 border-primary-100 hover:bg-black text-primary transition-all duration-200 ease-linear cursor-pointer group ${
         isScrollDown
           ? "opacity-100 visible translate-y-0"
           : "opacity-0 invisible translate-y-24"
       }`}
     >
       <span className="flex flex-col items-center justify-center px-2 py-5 group-hover:pt-8 transition-all duration-200 ease-linear">
-        <FaArrowUpLong className="mb-4 group-hover:-translate-y-3 transition-all duration-200 ease-linear" />
+        <FaArrowUpLong className="mb-4 text-lg group-hover:-translate-y-3 transition-all duration-200 ease-linear" />
         {textVertical.map((char, index) => (
-          <span key={index} className="text-lg uppercase font-elCamino">
+          <span key={index} className="text-base uppercase font-elCamino">
             {char === " " ? "\u00A0" : char}
           </span>
         ))}
